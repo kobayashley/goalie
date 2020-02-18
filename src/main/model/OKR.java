311ef3b31fdfaafb4d614698b18d5b88b0c5e5a9
kobayashley <a.mkobayashi@hotmail.com>
2020-02-17 23:38:52 -0800

    P2 progress in OKR Class, main method, some problems still with Calendar and Date Classes. Added labels to specify project requirements in the code

--- src/main/model/OKR.java
+++ src/main/model/OKR.java
@@ -4,6 +4,7 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class OKR {
+    //Make at least one class named appropriately for your application
 
     private Date startDate;
     private Date endDate;
@@ -14,11 +15,32 @@ public class OKR {
     private String assignedTo;
 
     public OKR(Date deadline, String name){
+        //Declare a field and use it (not necessarily at the same time!)
         startDate = Calendar.getInstance().getTime();
         todaysDate = Calendar.getInstance().getTime();
         endDate = deadline;
         completed = false;
         assignedTo = name;
 
+    }
+
+    public void setStartDate(Date date){
+        this.startDate = date;
+    }
+
+    public void setEndDate(Date date){
+        this.endDate = date;
+    }
+
+    public void setTodaysDate(){
+        this.todaysDate = Calendar.getInstance().getTime();
+    }
+
+    public void setCompleted(){
+        this.completed = true;
+    }
+
+    public void setAssignedTo(String name){
+        this.assignedTo = name;
     }
 }
--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,9 +1,23 @@
 package main.ui;
 
+import main.model.OKR;
+
+import java.util.Calendar;
+import java.util.Date;
+
 public class Main {
+    //Make a main method that gets things going
 
     public static void main(String[] args) {
         System.out.println("getting things goin");
+        Calendar deadline1 = new Calendar();
+        long deadline1Time = deadline1.getTimeInMillis();
+        Date deadlineDate = new Date(deadline1Time);
+
+        //Construct an object (Note: this should probably happen within your main method)
+        //Pass a parameter
+        //Use a local variable
+        OKR okr = new OKR(deadlineDate, "Ashley");
 
         defineOKR();
         updateProgress();
@@ -18,3 +32,6 @@ public class Main {
     }
 }
 
+//ask Noa: Date is deprecated, how to convert between Calendar object and Date object
+//also if passing a parameter in the constructor is also an example of a local variable,
+// can this count for both requirements as met?
