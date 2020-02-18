c74868098ba951178fe9920df4dafd5cf5624ace
kobayashley <a.mkobayashi@hotmail.com>
2020-02-17 21:47:13 -0800

    adding src

---
+++ src/main/ui/Main.java
@@ -0,0 +1,18 @@
+package main.ui;
+
+public class Main {
+
+    public static void main(String[] args) {
+        defineOKR();
+        updateProgress();
+    }
+
+    public static void defineOKR() {
+        System.out.println("this is a new OKR!");
+    }
+
+    public static void updateProgress(){
+        System.out.println("this OKR status updated");
+    }
+}
+
