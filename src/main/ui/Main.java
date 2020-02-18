754224dd1ce540ca7b510e70fdd4d9f53e9e2d32
kobayashley <a.mkobayashi@hotmail.com>
2020-02-17 22:07:40 -0800

    actual deliverable 1 changes

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -2,6 +2,17 @@ package main.ui;
 
 public class Main {
 
-    
+    public static void main(String[] args) {
+        defineOKR();
+        updateProgress();
+    }
+
+    public static void defineOKR() {
+        System.out.println("this is a new OKR!");
+    }
+
+    public static void updateProgress(){
+        System.out.println("this OKR status updated");
+    }
 }
 
