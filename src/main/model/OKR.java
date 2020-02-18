73a8bc89555835efa9701df360fe8a7a6be93cf2
kobayashley <a.mkobayashi@hotmail.com>
2020-02-17 22:53:54 -0800

    some initial Class OKR variables

---
+++ src/main/model/OKR.java
@@ -0,0 +1,14 @@
+package main.model;
+
+import java.util.Date;
+
+public class OKR {
+
+    private Date startDate;
+    private Date endDate;
+    private Date todaysDate;
+
+    private boolean completed;
+
+    private String assignedTo;
+}
