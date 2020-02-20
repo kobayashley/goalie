74ac4a5e0f7ddaeac93a269c6ad34016dbd58ba9
kobayashley <a.mkobayashi@hotmail.com>
2020-02-19 17:23:15 -0800

    got rid of initial comments, no helper methods needed extraction in KRTest

--- src/main/tests/KeyResultTest.java
+++ src/main/tests/KeyResultTest.java
@@ -18,50 +18,37 @@ public class KeyResultTest {
 
     @Test
     public void markCompletedNotAlreadyCompletedTest(){
-        //check that kr is not marked completed already
         assertFalse(kr.isCompleted());
-        //mark it completed
         kr.markCompleted();
-        //check that kr is now marked completed
         assertTrue(kr.isCompleted());
     }
 
     @Test
     public void markCompletedAlreadyCompletedTest(){
-        //check that kr is already marked completed
         kr.markCompleted();
         assertTrue(kr.isCompleted());
-        //mark it completed
         kr.markCompleted();
-        //check that kr is still marked completed
         assertTrue(kr.isCompleted());
     }
 
     @Test
     public void markIncompleteAlreadyCompleteTest(){
-        //check that kr is already marked completed
         kr.markCompleted();
         assertTrue(kr.isCompleted());
-        //mark it incomplete
         kr.markIncomplete();
-        //check that kr is now marked incomplete
         assertFalse(kr.isCompleted());
     }
 
     @Test
     public void markIncompleteAlreadyIncompleteTest(){
-        //check that kr is already marked incomplete
         assertFalse(kr.isCompleted());
-        //mark it incomplete
         kr.markIncomplete();
-        //check that kr is still incomplete
         assertFalse(kr.isCompleted());
     }
 
     @Test
     public void isCompletedTrueTest(){
         kr.markCompleted();
-        //check that kr is already marked complete
         assertTrue(kr.isCompleted());
 
     }
@@ -74,21 +61,15 @@ public class KeyResultTest {
 
     @Test
     public void setMetricFromEmptyTest(){
-        //check that kr has an empty metric
         assertEquals(kr.getMetric(), "");
-        //set the metric to something
         kr.setMetric("test metric");
-        //check that kr's metric has been set to that.
         assertEquals(kr.getMetric(), "test metric");
     }
 
     @Test
     public void getMetricTest(){
-        //check that kr has an empty metric
         assertEquals(kr.getMetric(), "");
-        //set the metric to something
         kr.setMetric("test metric");
-        //check that kr's metric has been set to that.
         assertEquals(kr.getMetric(), "test metric");
     }
 }
