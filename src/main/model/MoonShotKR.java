94c9dba790159158ec5aab7f25b5bfbfa4cb5f48
kobayashley <a.mkobayashi@hotmail.com>
2020-02-21 16:06:27 -0800

    added requires, modifies, effects clauses to MoonShotKR

--- src/main/model/MoonShotKR.java
+++ src/main/model/MoonShotKR.java
@@ -6,30 +6,42 @@ public class MoonShotKR {
     //Moonshots are stretch-goals in the form of Key Results, towards an overall Objective
 
     private boolean wellPositioned;
-    private boolean extraResources;
+    private boolean extraResourcesAvailable;
     private ArrayList<String> milestones;
 
-    
+    //MODIFIES: this
+    //EFFECTS: sets this wellPositioned to true or false, given as param.
     public void setWellPositioned(boolean b){
         this.wellPositioned = b;
     }
 
+    //MODIFIES: this
+    //EFFECTS: sets this extraResourcesAvailable to true or false, given as param.
     public void setExtraResources(boolean b){
-        this.extraResources = b;
+        this.extraResourcesAvailable = b;
     }
 
+    //REQUIRES: non-empty, valid String describing a milestone
+    //MODIFIES: this
+    //EFFECTS: adds m to this list of milestones
     public void addMilestone(String m){
         this.milestones.add(m);
     }
 
+    //REQUIRES: wellPositioned is not null
+    //EFFECTS: returns whether this moonshot is well-positioned
     public boolean isWellPositioned(){
         return this.wellPositioned;
     }
 
-    public boolean isExtraResources(){
-        return this.extraResources;
+    //REQUIRES: extraResourcesAvailable is not null
+    //EFFECTS: returns whether there are extra resources available to accomplish this moonshot
+    public boolean isExtraResourcesAvailable(){
+        return this.extraResourcesAvailable;
     }
 
+    //REQUIERS: non-empty list of milestones
+    //EFFECTS: returns the milestone at index i
     public String getMilestone(int i){
         return this.milestones.get(i);
     }
